9e475be8fd7295757d544f686f73b82ad076507e
Ashley Wu <ashleywu2004@gmail.com>
2025-10-23 23:00:55 -0700

    chore: set up components folder and placeholders

---
+++ frontend/src/content/ui/renderSearchBar.js
@@ -0,0 +1,7 @@
+/**
+ * Renders the SearchBar component into Piazza page via Shadow DOM root
+ * Visible at the top of the page as a temporary placeholder UI
+ */
+export function renderSearchBar() {
+    // TODO
+}
